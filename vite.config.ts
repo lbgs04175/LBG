53ee9d389a77c78e3ce032a81602c6e2196bf670
TKBso <lbgs04175@gmail.com>
2026-09-24 09:03:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/tkbtanhoa/',
+    base: '/LBG/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
